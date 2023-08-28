7d8e1d62d58bd4569a2ba8fad57454bf0b2d4458
alaaa <111141694+aalaahamam@users.noreply.github.com>
2023-08-28 13:55:08 +0200

    last

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,86 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:newsapp/Data/cubit/Getnews/getnews_cubit.dart';
 import 'package:newsapp/Data/models/getnews_Mode.dart';
+import 'package:newsapp/firebase/firebase_options.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 import 'package:newsapp/screens/frist.dart';
 import 'package:newsapp/screens/second.dart';
 
-void main() {
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  if (kDebugMode) {
+    print(message.messageId);
+  }
+}
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+  // Initialize FlutterLocalNotificationsPlugin
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  final InitializationSettings initializationSettings =
+      InitializationSettings(
+    android: initializationSettingsAndroid,
+      );
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  get flutterLocalNotificationsPlugin => null;
+
+Future<void> showHeadNotification() async {
+  const AndroidNotificationDetails androidPlatformChannelSpecifics =
+      AndroidNotificationDetails(
+    'your_channel_id',
+    'your_channel_name',
+    importance: Importance.max,
+    priority: Priority.high,
+    showWhen: false,
+  );
+  const NotificationDetails platformChannelSpecifics =
+      NotificationDetails(android: androidPlatformChannelSpecifics);
+  await flutterLocalNotificationsPlugin.show(
+    0,
+    'Head Notification Title',
+    'Head Notification Body',
+    platformChannelSpecifics,
+  );
+}
+
+
+
+@override
+void initState() {
+  super.initState();
+  showHeadNotification();
+}
+
+
+
 
   // This widget is the root of your application.
   @override
@@ -26,7 +96,7 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         theme: ThemeData(),
         // ignore: prefer_const_constructors
-        home:  FirtScreen( ),
+        home: FirtScreen( ),
     ));
   }
 }
